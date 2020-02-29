942bfc5966f85bccb94a766443599ec69040e8b8
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-02-29 12:47:43 -0300

    Criado classe Retangulo

--- src/main/java/exercicios/Retangulo.java
+++ src/main/java/exercicios/Retangulo.java
@@ -1,8 +1,15 @@
 package exercicios;
 
 public class Retangulo {
-    public Double base;
-    public Double altura;
+    private Double base;
+    private Double altura;
+
+    public Retangulo(Double base, Double altura) {
+        if (( base > 0.0 ) && (altura > 0.0) ) {
+            this.base = base;
+            this.altura = altura;
+        }
+    }
 
     public Double calcularArea() {
         return base * altura;
@@ -10,5 +17,21 @@ public class Retangulo {
 
     public Double calcularPerimetro() {
         return 2 * (base + altura);
+    }
+
+    public void setBase(Double base) {
+        if( base > 0.0 ) this.base = base;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setAltura(Double altura) {
+        if( altura > 0.0 ) this.altura = altura;
+    }
+
+    public Double getAltura() {
+        return altura;
     }
 }
