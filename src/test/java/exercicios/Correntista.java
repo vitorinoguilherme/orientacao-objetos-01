34cf4dd2709598e5b78261529598e7b349844ee9
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-02-29 13:39:51 -0300

    Criado classe Correntista

---
+++ src/test/java/exercicios/Correntista.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Correntista {
+    private String codigo;
+    private String nome;
+    private String email;
+    private int telefone;
+
+    public Correntista(String codigo, String nome, String email, int telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.codigo = codigo;
+        this.telefone = telefone;
+    }
+}
