0877ed3623d6d7fa26e7612559356814ef81a995
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-02-29 15:08:00 -0300

    Criado construtor de chamada

--- src/main/java/exercicios/Funcionario.java
+++ src/main/java/exercicios/Funcionario.java
@@ -6,6 +6,12 @@ public class Funcionario {
     public Integer idade;
     public Double salario;
 
+    public Funcionario(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
     public String obterDados() {
         return String.format("%s, %d anos (%s) %n", nome, idade, email);
     }
